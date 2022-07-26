a04559a136be9c5f80fe4fa8cdc83f90cb84b005
Wolke Letzet <wolke.letzet@gmail.com>
2022-07-25 21:55:43 -0400

    Prototipo

--- tester.py
+++ tester.py
@@ -1,37 +1,32 @@
+from gettext import find
+from multiprocessing.connection import wait
 from pprint import pprint
+from csv import excel
+from math import e
+from pprint import pprint
+from time import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from to_excel import ToExcel
+from time import sleep
 
-url = "https://www.hostelworld.com/s?q=Valparaiso,%20Chile&country=Chile&city=Valparaiso&type=city&id=1868&from=2022-07-25&to=2022-07-28&guests=2&HostelNumber=&page=1"
 
-class HostelScraper:
+
+class WebDriver():
+   
    def __init__(self):
       self.options = Options()
       #self.options.add_argument("--headless")
       self.options.add_argument("--disable-gpu")
       self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.options)
       self.wait = WebDriverWait(self.driver, 10)
-      
    
-   def get_hostel_coments_url(self, url,limit=1):
-      self.go_to_url_by_class_name(url, "info")
-      review_url = self.driver.find_elements(By.CSS_SELECTOR, "div.bottom-rating > a")
-      urls= [x for x in review_url if x.get_attribute("href")]
-      urls= [x.get_attribute("href") for x in urls]
-      return urls
-   
-   def go_to_url_by_class_name(self, url,class_name):
+   def go_to_url_by_class_name(self, url, class_name):
       self.driver.get(url)
       self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, class_name)))
-   
-   def go_to_url_by_css_selector(self, url,css_selector):
-      self.driver.get(url)
-      self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, css_selector)))
    
    def get_hostel_rate(self):      
       
@@ -48,31 +43,31 @@ class HostelScraper:
       score= [x for x in score if x] # remove empty strings
       score= [float(x) for x in score]# convert to float
       return score
+   def find_element_by_class_name(self, class_name):
+      return self.driver.find_element(By.CLASS_NAME, class_name)
+   def find_elements_by_class_name(self, class_name):
+      return self.driver.find_elements(By.CLASS_NAME, class_name)
+   def find_element_by_id(self, id):
+      return self.driver.find_element(By.ID, id)
+   def find_element_by_xpath(self, xpath):
+      return self.driver.find_element(By.XPATH, xpath)
+   def find_elements_by_css_selector(self, css_selector):
+      return self.driver.find_elements(By.CSS_SELECTOR, css_selector)       
+   def find_element_by_css_selector(self, css_selector):
+      return self.driver.find_element(By.CSS_SELECTOR, css_selector)
    
    def close(self):
       self.driver.close()
       self.driver.quit()
 
-driver= HostelScraper()
-urls=driver.get_hostel_coments_url(url)
-pprint(urls)
+#url= "https://www.hostelworld.com/pwa/hosteldetails.php/Casa-Volante-Hostal/Valparaiso/82633?from=2022-07-26&to=2022-07-29&guests=2&display=reviews"
+url="https://www.hostelworld.com/pwa/hosteldetails.php/Escarabajo-Hostel/Valparaiso/100905?from=2022-07-26&to=2022-07-29&guests=2&display=reviews"
+driver= WebDriver()
+driver.go_to_url_by_class_name(url, "pagination")
+date= driver.find_elements_by_css_selector("div.date>span")
+date= [x.text for x in date if "2022" in x.text or "2021" in x.text or "2020" in x.text]
 
-for url in urls:
-
-   driver.go_to_url_by_class_name(url, "pagination-next")
-   next_page= driver.driver.find_element(By.CLASS_NAME, "pagination-next")
-   score= driver.get_hostel_users_score()
-   rate= driver.get_hostel_rate()
-   pprint(rate)
-   while next_page.get_dom_attribute("class") != "pagination-item pagination-next disabled":
-      next_page.click()
-      score.extend(driver.get_hostel_users_score())
-      next_page = driver.driver.find_element(By.CLASS_NAME, "pagination-next")
       
-   pprint(score)
-   print("\n")
-
-
+print(date)
 
 driver.close()
-
