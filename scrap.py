32f7d180e002aa0820a5ab8e150738d44c026ba9
Wolke Letzet <wolke.letzet@gmail.com>
2022-09-01 14:41:01 -0400

    options

--- scrap.py
+++ scrap.py
@@ -50,7 +50,7 @@ class HostelScraper:
       filtershow= self.driver.find_element(By.CLASS_NAME, "filter.show")
       filtershow.find_element(By.CSS_SELECTOR, "ul > li:last-child").click()
 
-   def get_reviews_in_user_page(self,hostel_name):
+   def get_reviews_in_user_page(self,hostel_name,options=[True,True,True]):
       reviews_list=self.driver.find_elements(By.CSS_SELECTOR, "div.reviewlisting")
       
       reviews=[]
@@ -58,8 +58,13 @@ class HostelScraper:
          hostel_review=item.find_element(By.CSS_SELECTOR, "div.popupreviewlocation >a").text
          if hostel_review==hostel_name:
             review={'text':[], 'score':[], 'date':[], 'author':[],'author-details':[],'hostel':hostel_name,'rate':[]}
-            review['text']=item.find_element(By.CSS_SELECTOR, "div.reviewtext").text
-            review['score']=item.find_element(By.CSS_SELECTOR, "div.textrating").text
+            
+            if options[2] :
+               review['text']=item.find_element(By.CSS_SELECTOR, "div.reviewtext").text
+            
+            if options[0]:
+               review['score']=item.find_element(By.CSS_SELECTOR, "div.textrating").text
+            
             review['date']=item.find_element(By.CSS_SELECTOR, "span.reviewdate").text
             review['author']=item.find_element(By.CSS_SELECTOR, "li.reviewername").text
             reviewer_details=item.find_element(By.CSS_SELECTOR, "li.reviewerdetails").text
@@ -74,11 +79,14 @@ class HostelScraper:
             
       return reviews
    
+
+
+   
    def close(self):
       self.driver.close()
       self.driver.quit()
 
-def main(url="",excel_name="reviews.xlsx",options=None):
+def main(url="",excel_name="reviews.xlsx",options=[True,True,True,True]):
 
    driver= HostelScraper()
    links=driver.get_hostel_coments_url(url)
@@ -88,11 +96,11 @@ def main(url="",excel_name="reviews.xlsx",options=None):
 
    for link in links:
       driver.go_to_url_by_class_name(link, "pagination-next")
-      try:
-         driver.change_reviews_lang()
-      except:
-         driver.change_reviews_lang()
-         pass
+      if options[0]:
+         try:
+            driver.change_reviews_lang()
+         except:
+            pass
       review_list=driver.driver.find_elements(By.CSS_SELECTOR, "div.review-item")
       reviews=[]
       hostel_name=driver.driver.find_element(By.CSS_SELECTOR, "div.title-2").text
@@ -114,7 +122,7 @@ def main(url="",excel_name="reviews.xlsx",options=None):
             if reviews_num > 1:
                reviewer_url=item.find_element(By.CSS_SELECTOR, "div.user-review > ul > li:last-child > a").get_attribute("href")
                driver2.go_to_url_by_class_name(reviewer_url, "reviewdetails")
-               reviews=driver2.get_reviews_in_user_page(hostel_name)
+               reviews=driver2.get_reviews_in_user_page(hostel_name,options)
                
                try:
                   for rev in reviews:
@@ -123,6 +131,28 @@ def main(url="",excel_name="reviews.xlsx",options=None):
                      excel_book.wb.save(excel_name)
                except:
                   pass
+            elif reviews_num == 1 and options[3]:
+               review={'text':[], 'score':[], 'date':[], 'author':[],'author-details':[],'hostel':hostel_name,'rate':[0,0,0,0,0,0,0]}
+               review['author']=item.find_element(By.CSS_SELECTOR,"li.name").text
+               review["author-details"].append('Null')
+               
+               reviewer_details=item.find_element(By.CSS_SELECTOR,"li.details").text
+               reviewer_details=reviewer_details.split(",")
+               
+               for field in reviewer_details:
+                  review['author-details'].append(field.strip())
+               
+               review["text"]=item.find_element(By.CSS_SELECTOR,"div.review-notes").text
+               review['score']=item.find_element(By.CSS_SELECTOR,"div.score").text
+               review['date']=item.find_element(By.CSS_SELECTOR,"div.date > span").text
+               
+               try:
+                  excel_book.add_review(review)
+                  excel_book.wb.save(excel_name)
+               except:
+                  pass
+               
+               
          if "disabled" in next_page.get_attribute("class"):
             break
          else:
