757e5160372a5715131b02c498ead206686a83c8
Claudio Pastenes <claudio.pastenes@informaticapucv.cl>
2022-10-13 22:19:24 -0300

    ss

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import tkinter as tk
 from Scraper import Scraper
 from gg import GUI
 
+
 def schedule_check(t):
     """
     Programar la ejecución de la función `check_if_done()` dentro de 
     un segundo.
     """
-    root.ventana.after(100, check_if_done, t)
+    root.after(100, check_if_done, t)
+
+
 def check_if_done(t):
     # Si el hilo ha finalizado, restaruar la pantalla principal
     if not t.is_alive():
@@ -20,17 +23,20 @@ def check_if_done(t):
         gui.updateProgressbar(scraper.counter)
         schedule_check(t)
 
+
 def scraping():
     scraper.setPropertiesIDs(gui.getCity())
     gui.build_progressBar_window(scraper.properties.__len__())
-    th  =Thread(target=scraper.mainloop , args=[gui.getSavePath(),gui.getCity()])
+    th = Thread(target=scraper.mainloop, args=[
+                gui.getSavePath(), gui.getCity()])
     th.start()
-    
+
     schedule_check(th)
-    
+
+
 path = os.path.dirname(os.path.realpath(__file__))
-root =tk.Tk()
-gui = GUI(path,root)
+root = tk.Tk()
+gui = GUI(path, root)
 gui.setMain()
 scraper = Scraper()
 
